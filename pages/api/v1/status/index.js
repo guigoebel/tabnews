f149c7b18ec6d463870b1df0ad68e5ccff6ffa0c
Guilherme Goebel <guigoebel@gmail.com>
2026-04-21 11:37:28 -0300

    add `database.js` e env local

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ message: "API is working" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result);
+  response.status(200).json({ message: "API is working", result });
 }
 
 export default status;
